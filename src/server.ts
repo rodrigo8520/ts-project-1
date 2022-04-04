cc36bc6fbdc3e9f346f5b6cc3f815a89eb44d01e
rodrigo8520 <rodrigorojasaragon@gmail.com>
2022-04-04 12:35:33 -0400

    Ejemplo sumar en server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,14 +13,50 @@ class Server {
 
     constructor() {
 
-        this.app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+        this.app.get('/', (req, res) => {
+            res.send('Express + TypeScript Server')
+        });
 
-        function suma (numero1:number, numero2:number) :number {
-            return numero1+numero2;
-        }
-        console.log(suma(10,5));
+        this.app.get('/sumar/:valor1/:valor2', (req, res) => {
+            let var1 = req.params.valor1;
+            let var2 = req.params.valor2;
+    
+
+            let aus1 = parseInt(var1)
+            let aus2 = parseInt(var2)
+            
+            const respuesta = this.sumar(aus1,aus2);
+            res.send('el numero es '+ respuesta);
+            //res.send('el numero es '+dos);
+        });
+
+        
+
+        // function suma (numero1:number, numero2:number) {
+        //     return numero1+numero2;
+        // }
+        // console.log(suma(10,5));
+
+        // const suma2 = (num1:number, num2:number) :number => {
+        //     return num1+num2;
+        // }
 
     }
+
+    sumar (num1: number, num2: number): number   {
+        
+            return num1 + num2;
+        }
+        
+
+    resta() {
+        const reta = (num1: number, num2: number): number => {
+            return num1 - num2;
+        }
+        console.log(reta(10, 5));
+        
+    }
+
 
     listen() {
         this.app.listen(this.port, () => {
