cdf7ff661d86a13ad6797dbaa419cf9823984bf3
rodrigo8520 <rodrigorojasaragon@gmail.com>
2022-04-04 19:32:43 -0400

    Ejercicio Salario

--- src/server.ts
+++ src/server.ts
@@ -18,19 +18,30 @@ class Server {
         });
 
         this.app.get('/sumar/:valor1/:valor2', (req, res) => {
+
             let var1 = req.params.valor1;
             let var2 = req.params.valor2;
-    
+
 
             let aus1 = parseInt(var1)
             let aus2 = parseInt(var2)
-            
-            const respuesta = this.sumar(aus1,aus2);
-            res.send('el numero es '+ respuesta);
+
+            const respuesta = this.sumar(aus1, aus2);
+            res.send('el numero es ' + respuesta);
             //res.send('el numero es '+dos);
         });
 
-        
+        this.app.get('/horaTrabajo/:valor1/:valor2', (req, res) => {
+
+            let var1 = req.params.valor1;
+            let var2 = req.params.valor2;
+
+            let aus1 = parseInt(var1)
+            let aus2 = parseInt(var2)
+
+            const respuesta = this.horaTrabajo(aus1, aus2);
+            res.send('Tu salario es ' + respuesta);
+        })
 
         // function suma (numero1:number, numero2:number) {
         //     return numero1+numero2;
@@ -39,22 +50,40 @@ class Server {
 
         // const suma2 = (num1:number, num2:number) :number => {
         //     return num1+num2;
-        // }
+        // }procedimiento retorna algo
 
     }
 
-    sumar (num1: number, num2: number): number   {
-        
-            return num1 + num2;
-        }
-        
+    sumar(num1: number, num2: number): number {
 
-    resta() {
-        const reta = (num1: number, num2: number): number => {
-            return num1 - num2;
-        }
-        console.log(reta(10, 5));
+        return num1 + num2;
+    }
+
+
+    hora(hora: number, tarifa: number): number {
+
+        let resultadoHora 
+        return resultadoHora = hora * tarifa;
+    }
+
+    horaExtra(hora: number, tarifa: number): number {
+
+        return hora * (tarifa + tarifa/2)
+    }
+
+    horaTrabajo(horas: number, tarifa: number): number {
         
+        let salarios
+
+        if (horas > 40) {
+            salarios = this.horaExtra(horas, tarifa)
+            
+
+        } else if (horas <= 40) {
+            salarios = this.hora(horas, tarifa)
+
+        }
+        return salarios;
     }
 
 
